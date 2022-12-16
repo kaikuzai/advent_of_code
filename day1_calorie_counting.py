4c016da507a14de01e4a4c2c08c873684674ca4a
Dylan Okyere <dylan.okyere@gmail.com>
2022-12-17 00:38:04 +0100

    general framework for the project

--- day1_calorie_counting.py
+++ day1_calorie_counting.py
@@ -1,1 +1,18 @@
-something
+# Read the txt file 
+import urllib.request
+
+url = 'https://adventofcode.com/2022/day/1/input'
+
+urllib.request.urlretrieve(url, 'input.txt')
+
+with open('input.txt', 'r') as f:
+    content = f.read()
+
+print(content)
+# Sepperate all the indexes into groups by using the blank spaces as deviders
+
+# Assign each group and aggregate the sum values of that group
+
+# Find the group with the highest aggregation
+
+# Print highest aggregation
