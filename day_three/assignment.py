670528aece07f9e3097673819d45e098163441b0
Dylan Okyere <dylan.okyere@gmail.com>
2022-12-20 12:57:44 +0100

    final solution day 3 part one

--- day_three/assignment.py
+++ day_three/assignment.py
@@ -23,14 +23,26 @@ for i in range(len(stripped)):
     second_halve.append(stripped[i][midsection[i]:])
 
 # compare the values in each input half
-def similar_characters(string1, string2):
-    set1 = set(string1)
-    set2 = set(string2)
-    common_characters = set1.intersection(set2)
-    same_characters = list(common_characters)
-    print(f'in string number {i} the similar character is {same_characters}')
+def similar_characters(list1, list2):
+    character_list = []
+    for i in range(len(list1)):
+        set1 = set(list1[i])
+        set2 = set(list2[i])
+        common_characters = set1.intersection(set2)
+        same_characters = list(common_characters)
+        character_list += same_characters
+    return character_list
+    
+character_list = similar_characters(first_halve, second_halve)
 
-for i in range(len(stripped)):
-    similar_characters(first_halve[i], second_halve[i])
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-print(first_halve[-2] + '--' + second_halve[-2])
+priority_points = 0 
+
+try:
+    for i in character_list:
+        priority_points += alphabet.index(i) + 1
+except ValueError:
+    print(f"Character {i} not found in alphabet")
+
+print(priority_points)
