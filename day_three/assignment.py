38f9134a611c7d22320e89305e1e921f05fbc85a
Dylan Okyere <dylan.okyere@gmail.com>
2022-12-20 12:09:17 +0100

    a bit further

--- day_three/assignment.py
+++ day_three/assignment.py
@@ -14,15 +14,23 @@ midsection = []
 for i in range(len(stripped)):
     midsection.append(int(len(stripped[i]) / 2))
 
+# get a list with the first and second section of each input
 first_halve = []
 second_halve = []
 
-print(stripped[0])
 for i in range(len(stripped)):
     first_halve.append(stripped[i][:midsection[i]])
     second_halve.append(stripped[i][midsection[i]:])
 
-print(stripped[0])
-print(first_halve[0] + second_halve[0])
-print(first_halve[0])
-print(second_halve[0])
+# compare the values in each input half
+def similar_characters(string1, string2):
+    set1 = set(string1)
+    set2 = set(string2)
+    common_characters = set1.intersection(set2)
+    same_characters = list(common_characters)
+    print(f'in string number {i} the similar character is {same_characters}')
+
+for i in range(len(stripped)):
+    similar_characters(first_halve[i], second_halve[i])
+
+print(first_halve[-2] + '--' + second_halve[-2])
