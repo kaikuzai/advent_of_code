8d448b6c3cfd8220502fe17fbef238964599536d
Dylan Okyere <dylan.okyere@gmail.com>
2022-12-20 11:57:25 +0100

    append values in list

--- day_three/assignment.py
+++ day_three/assignment.py
@@ -1,10 +1,28 @@
-
-
-
+#open and read the input file 
 with open('day_three/inputs.txt','r') as f:
     content = f.readlines()
 
-for words in content:
-    middle_point = len(words) / 2
-    print(middle_point)
+# strip the last character of which is an \n characture 
+stripped = []
 
+for i in content:
+    stripped.append(i[:-1])
+
+#find the midsection of each input item 
+midsection = []
+
+for i in range(len(stripped)):
+    midsection.append(int(len(stripped[i]) / 2))
+
+first_halve = []
+second_halve = []
+
+print(stripped[0])
+for i in range(len(stripped)):
+    first_halve.append(stripped[i][:midsection[i]])
+    second_halve.append(stripped[i][midsection[i]:])
+
+print(stripped[0])
+print(first_halve[0] + second_halve[0])
+print(first_halve[0])
+print(second_halve[0])
